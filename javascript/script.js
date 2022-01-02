07ffc1e4b28826247b6a1156598db3acd56f1ab6
MrWerton <mr.werton@gmail.com>
2022-01-01 21:57:18 -0300

    Builded Product page

--- javascript/script.js
+++ javascript/script.js
@@ -18,3 +18,22 @@ btnClosed.addEventListener("click",closeMenu)
 for(let link of links){
     link.addEventListener('click', closeMenu)
 }
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+
+  });
