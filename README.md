bc5d81c38e1f0d4d13fa5c245a9d98f69e20f83d
MrWerton <mr.werton@gmail.com>
2022-01-03 10:25:43 -0300

    back on the project, i changed some concepts and finalized product and testimonial

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # Project Housan
 ## Technologies:
-* Scss
+* css
 * javascript
 * html
+
+---
+## Library
+* scss
+* swiperjs
+---
 
     link of design: https://dribbble.com/shots/15761083/attachments/7566804?mode=media
--- javascript/script.js
+++ javascript/script.js
@@ -27,13 +27,20 @@ const swiper = new Swiper('.swiper', {
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
+      type: 'bullets',
     },
   
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+  
   
 
   });
+
+const container = document.querySelectorAll(".container div");
+
+  /* addEventListener('click', ()=>{
+for(let div of container){
+  console.log("click")
+   div.classList.toggle('active')
+}
+})
+ */
